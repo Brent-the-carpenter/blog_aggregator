ec650afbbbf83c7201056f856d2ce9f1357076e0
Brent-the-carpenter <beh337@gmail.com>
2024-11-03 11:46:01 -0500

    Refactored to match boot.dev file structor to help with consistency and also to practice writting good idiomatic go code.

--- commands.go
+++ commands.go
@@ -20,7 +20,7 @@ func (c *commands) register(name string, f func(*state, command) error) {
 func (c *commands) run(s *state, cmd command) error {
 	cmdHandler, ok := c.registeredCommands[cmd.name]
 	if !ok {
-		return fmt.Errorf("command:%s does not exists. Please register command.", cmd.name)
+		return fmt.Errorf("command:%s does not exists. Please register command", cmd.name)
 	}
 	return cmdHandler(s, cmd)
 
--- handler_user.go
+++ handler_user.go
@@ -4,7 +4,41 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/Brent-the-carpenter/blog_aggregator/internal/database"
+	"github.com/google/uuid"
 )
+
+func handleRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: register <username>")
+	}
+	name := cmd.args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
+
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+
+	}
+
+	err = s.config.SetUser(user.Name)
+	if err != nil {
+
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User created successfully:")
+	printUser(user)
+
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 
@@ -23,4 +57,9 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	fmt.Printf("Username:%s has been logged in.\n", username)
 	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
